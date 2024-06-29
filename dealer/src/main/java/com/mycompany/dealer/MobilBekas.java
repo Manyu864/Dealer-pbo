eb1c37525e8ce991985b8d9087f33946d543bb25
Manyu864 <abim50057@gmail.com>
2024-06-29 15:48:50 +0700

    Dealer Done

---
+++ dealer/src/main/java/com/mycompany/dealer/MobilBekas.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dealer;
+
+/**
+ *
+ * @author Asus
+ */
+public class MobilBekas extends Mobil implements Kendaraan{
+    private int tahun;
+    private double harga;
+
+    public MobilBekas(String merek, String model, int tahun, double harga) {
+        super(merek, model);
+        this.tahun = tahun;
+        this.harga = harga;
+    }
+    public int getTahun() {
+        return tahun;
+    }
+    public double getHarga() {
+        return harga;
+    }
+//ini adalah Notation yang menunjujjkan implementasi
+// dari metode info() yang dideklarasikan di interface
+    @Override
+    public void info() {
+        System.out.println("Mobil Bekas: " + getMerek() + " " + getModel() + " ("
+    + tahun + ")");
+    }
+
+    public String getMerek() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getModel() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
+    }
+}
